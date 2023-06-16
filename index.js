f7fc7333f95cccc2a4bb0f04f625072544055b60
Murassa <fatima@gmail.com>
2023-06-16 16:07:02 +1000

    router file

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const cors = require('cors');
 const methodOverride = require('method-override');
 // const { v4: getId } = require('uuid');
 const mongoose = require('mongoose');
-const Product = require('./models/products');
-const Subscription = require('./models/subscriptions');
- 
+
 // Connect to MongoDB
 main().catch(err => console.log(err));
 
@@ -24,77 +22,78 @@ app.use(cors());
 app.use(express.json()); // For parsing JSON responses
 app.use(express.urlencoded({ extended: true })); // For parsing Form urlencoded responses
 app.use(methodOverride('_method'));
+app.use(require('./router.js')); // For requiring routes
 
 // Set
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// GET Routes
-app.get('/', (req, res) => { //get index page
-  res.render('index');
-});
+// // GET Routes
+// app.get('/', (req, res) => { //get index page
+//   res.render('index');
+// });
 
-app.get('/products', async (req, res) => { //get all products
-  const products = await Product.find({});
-  console.log(products);
-  res.render('products/allProducts', { products });
-});
+// app.get('/products', async (req, res) => { //get all products
+//   const products = await Product.find({});
+//   console.log(products);
+//   res.render('products/allProducts', { products });
+// });
 
-app.get('/products/new', (req, res) => { //get form for creating a product
-  res.render('products/createProduct');
-});
+// app.get('/products/new', (req, res) => { //get form for creating a product
+//   res.render('products/createProduct');
+// });
 
-app.get('/products/:id', async (req, res) => {
-  const { id } = req.params;
-  const product  = await Product.findById(id);
-  console.log(product);
-  res.render('products/productDetails', { product });
-});
+// app.get('/products/:id', async (req, res) => {
+//   const { id } = req.params;
+//   const product  = await Product.findById(id);
+//   console.log(product);
+//   res.render('products/productDetails', { product });
+// });
 
-app.get('/products/:id/edit', async (req, res) => { //get form for editing a product
-  const { id } = req.params;
-  const product = await Product.findById(id);
-  res.render('products/editProduct', { product });
-});
+// app.get('/products/:id/edit', async (req, res) => { //get form for editing a product
+//   const { id } = req.params;
+//   const product = await Product.findById(id);
+//   res.render('products/editProduct', { product });
+// });
 
-app.get('/subscribers', async (req, res) => { //get all products
-  const subscribers = await Subscription.find({});
-  console.log(subscribers);
-  res.render('customers/subscribers', { subscribers });
-});
+// app.get('/subscribers', async (req, res) => { //get all products
+//   const subscribers = await Subscription.find({});
+//   console.log(subscribers);
+//   res.render('customers/subscribers', { subscribers });
+// });
 
-// POST routes
+// // POST routes
 
-app.post('/products', async (req, res) => { //post a newly created product
-  const newProduct = new Product(req.body);
-  await newProduct.save();
-  res.redirect(`/products/${newProduct.id}`);
-});
+// app.post('/products', async (req, res) => { //post a newly created product
+//   const newProduct = new Product(req.body);
+//   await newProduct.save();
+//   res.redirect(`/products/${newProduct.id}`);
+// });
 
-app.post('/api/subscription', async (req, res) => {  // Subscribed emails intake
-  try {
-    // Extract the data from the request body
-    const { email } = req.body;
-    console.log(email);
-    //   Create an instance of the subscription model
-    const subscription = new Subscription({ email :email });
-    // Save the data to the database
-    const savedEmail = await subscription.save();
-    console.log(email);
-    res.status(201).json(savedEmail);
-  } catch (error) {
-        console.error('Failed to save email', error);
-        res.status(500).json({ error: 'Server Error' });
-      }
-});
+// app.post('/api/subscription', async (req, res) => {  // Subscribed emails intake
+//   try {
+//     // Extract the data from the request body
+//     const { email } = req.body;
+//     console.log(email);
+//     //   Create an instance of the subscription model
+//     const subscription = new Subscription({ email :email });
+//     // Save the data to the database
+//     const savedEmail = await subscription.save();
+//     console.log(email);
+//     res.status(201).json(savedEmail);
+//   } catch (error) {
+//         console.error('Failed to save email', error);
+//         res.status(500).json({ error: 'Server Error' });
+//       }
+// });
 
-// Put Routes
+// // Put Routes
 
-app.put('/products/:id', async (req, res) => { // Edit Product
-  const { id } = req.params;
-  const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true });
-  res.redirect(`/products/${product.id}`);
-})
+// app.put('/products/:id', async (req, res) => { // Edit Product
+//   const { id } = req.params;
+//   const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true });
+//   res.redirect(`/products/${product.id}`);
+// })
 
 
 // Start the server
---
+++ router.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('./models/products');
+const Subscription = require('./models/subscriptions');
+
+// GET Routes
+router.get('/', (req, res) => { //get index page
+    res.render('index');
+  });
+  
+  router.get('/products', async (req, res) => { //get all products
+    const products = await Product.find({});
+    console.log(products);
+    res.render('products/allProducts', { products });
+  });
+  
+  router.get('/products/new', (req, res) => { //get form for creating a product
+    res.render('products/createProduct');
+  });
+  
+  router.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product  = await Product.findById(id);
+    console.log(product);
+    res.render('products/productDetails', { product });
+  });
+  
+  router.get('/products/:id/edit', async (req, res) => { //get form for editing a product
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/editProduct', { product });
+  });
+  
+  router.get('/subscribers', async (req, res) => { //get all products
+    const subscribers = await Subscription.find({});
+    console.log(subscribers);
+    res.render('customers/subscribers', { subscribers });
+  });
+  
+  // POST routes
+  
+  router.post('/products', async (req, res) => { //post a newly created product
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct.id}`);
+  });
+  
+  router.post('/api/subscription', async (req, res) => {  // Subscribed emails intake
+    try {
+      // Extract the data from the request body
+      const { email } = req.body;
+      console.log(email);
+      //   Create an instance of the subscription model
+      const subscription = new Subscription({ email :email });
+      // Save the data to the database
+      const savedEmail = await subscription.save();
+      console.log(email);
+      res.status(201).json(savedEmail);
+    } catch (error) {
+          console.error('Failed to save email', error);
+          res.status(500).json({ error: 'Server Error' });
+        }
+  });
+  
+  // Put Routes
+  
+  router.put('/products/:id', async (req, res) => { // Edit Product
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true });
+    res.redirect(`/products/${product.id}`);
+  })
+
+  
+module.exports = router;
